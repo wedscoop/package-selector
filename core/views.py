01aab1725eb4bfdc52107cf5fbc94d502930ee6b
wedscoop <contact.wedscoop@gmail.com>
2026-04-21 03:22:51 +0530

    debug

--- core/views.py
+++ core/views.py
@@ -157,7 +157,7 @@ def view_plan(request, plan_id):
 def send_lead_email(name, phone, plan_id, event_date):
     try:
         resend.api_key = os.environ.get("RESEND_API_KEY")
-        print("KEY:", key)
+        print("KEY:", resend.api_key)
         resend.Emails.send({
             "from": "Wedscoop <onboarding@resend.dev>",
             "to": ["your-email@gmail.com"],
