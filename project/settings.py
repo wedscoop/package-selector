f26c3c652d257274b8724b183e2dc64884d53560
wedscoop <contact.wedscoop@gmail.com>
2026-05-09 00:47:12 +0530

    DEBUG=true

--- project/settings.py
+++ project/settings.py
@@ -72,8 +72,8 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        #"NAME": BASE_DIR / "db.sqlite3",
-        "NAME": "/app/data/db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        #"NAME": "/app/data/db.sqlite3",
 
     }
 }
@@ -93,8 +93,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 print("STATIC DIR:", STATICFILES_DIRS)
 
 ALLOWED_HOSTS = ["*"]  # temp, we’ll tighten later
-
-DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = [
     "https://package-selector-production.up.railway.app",
